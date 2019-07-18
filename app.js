e98fabc481e7b95ab74fceaa3d3277d041873bd8
Ajay <artstrom.2013@gmail.com>
2019-07-18 12:17:53 +0530

    false route handelling

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(compression())  // compress all responses
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -26,6 +27,7 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
+  return res.status(404).send({ message: 'Route'+req.url+' Not found.' });
   next(createError(404));
 });
 
